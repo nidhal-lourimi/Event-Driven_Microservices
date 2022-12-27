305a6fc9518d332db217dd9f4cdf3a567708c215
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-12-27 13:08:30 +0000

    minor change to expose running port and verifying load balancing work

--- product/src/main/java/com/nidhallourimi/contoller/ProductsController.java
+++ product/src/main/java/com/nidhallourimi/contoller/ProductsController.java
@@ -1,11 +1,15 @@
 package com.nidhallourimi.contoller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("products")
 public class ProductsController {
+    @Autowired
+    Environment environment;
     @PostMapping
     public String createProduct(){
         return "HTTP POSt Handled";
@@ -13,7 +17,7 @@ public class ProductsController {
 
     @GetMapping
     public String getProduct(){
-        return "HTTP GEt Handled";
+        return "HTTP GEt Handled with a port number ="+environment.getProperty("local.server.port");
 
     }
 
