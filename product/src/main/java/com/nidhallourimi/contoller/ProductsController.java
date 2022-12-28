21c1f2b6691839a59fac1e6a8eda98320d06087e
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2022-12-28 12:28:25 +0000

    create a product Command class and implement a command gateway to transfer object to the command bus

--- product/src/main/java/com/nidhallourimi/contoller/ProductsController.java
+++ product/src/main/java/com/nidhallourimi/contoller/ProductsController.java
@@ -1,19 +1,44 @@
 package com.nidhallourimi.contoller;
 
+import com.nidhallourimi.command.CreateProductCommand;
 import com.nidhallourimi.model.CreateProductRestModel;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("products")
 public class ProductsController {
-    @Autowired
+
     Environment environment;
+    CommandGateway commandGateway;
+    @Autowired
+    public ProductsController(Environment environment, CommandGateway commandGateway) {
+        this.environment = environment;
+        this.commandGateway = commandGateway;
+    }
+
+
+
     @PostMapping
     public String createProduct(@RequestBody CreateProductRestModel productRestModel){
-        return "HTTP POSt Handled and your product titles is "+ productRestModel.getTitle();
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .title(productRestModel.getTitle())
+                .price(productRestModel.getPrice())
+                .quantity(productRestModel.getQuantity())
+                .build();
+        String returnValue;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        }catch (Exception e){
+            returnValue =e.getLocalizedMessage();
+        }
+        return returnValue;
     }
 
     @GetMapping
