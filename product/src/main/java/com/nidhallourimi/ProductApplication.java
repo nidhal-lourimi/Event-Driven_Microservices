d625ea51dbd19acecf97c40cc55264a19dc480a7
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2023-01-03 18:15:24 +0000

    register message dispatch interceptor

--- product/src/main/java/com/nidhallourimi/ProductApplication.java
+++ product/src/main/java/com/nidhallourimi/ProductApplication.java
@@ -1,10 +1,14 @@
 package com.nidhallourimi;
 
+import com.nidhallourimi.command.interceptors.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
 import org.axonframework.eventsourcing.Snapshotter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -13,6 +17,10 @@ import org.springframework.stereotype.Component;
 public class ProductApplication {
     public static void main(String[]args){
         SpringApplication.run(ProductApplication.class,args);
+    }
+    @Autowired
+    public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus){
+    commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
     }
 }
 
