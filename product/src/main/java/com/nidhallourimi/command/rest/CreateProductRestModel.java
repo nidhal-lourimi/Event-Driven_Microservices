55fece5f62ab2031d6d671085df5d918eacf9cb9
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2023-01-03 16:32:53 +0000

    validating request body

--- product/src/main/java/com/nidhallourimi/command/rest/CreateProductRestModel.java
+++ product/src/main/java/com/nidhallourimi/command/rest/CreateProductRestModel.java
@@ -3,11 +3,18 @@ package com.nidhallourimi.command.rest;
 import lombok.Data;
 
 import javax.persistence.Column;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 
 @Data
 public class CreateProductRestModel {
+    @NotBlank(message = "Product title is a required field")
     private String title;
+    @Min(value = 1,message = "Price cannot be lower than 1 TND")
     private BigDecimal price;
+    @Min(value = 1 ,message = "Quantity cannot be lower than 1")
+    @Max(value = 1000,message = "Quantity cannot be higher than 1000")
     private Integer quantity;
 }
