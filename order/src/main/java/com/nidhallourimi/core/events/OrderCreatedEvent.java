368d8ee467792bbb1a8a77590fb90a2b68d51f1c
nidhal louremi <52574003+nidhal-lourimi@users.noreply.github.com>
2023-01-06 10:54:47 +0000

    creating orders-microservice with CQRS and Event Sourcing architecture

--- order/src/main/java/com/nidhallourimi/core/events/OrderCreatedEvent.java
+++ order/src/main/java/com/nidhallourimi/core/events/OrderCreatedEvent.java
@@ -1,9 +1,13 @@
 package com.nidhallourimi.core.events;
 
 import com.nidhallourimi.command.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderCreatedEvent {
     private String orderId;
     private String productId;
